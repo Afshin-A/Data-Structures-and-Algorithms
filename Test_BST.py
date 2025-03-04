5748510a895f0c2b23da03897afec8fc53d3b437
Afshin-A <86595208+Afshin-A@users.noreply.github.com>
2025-03-04 02:55:42 -0600

    Implemented PriorityQueue Working on BST

---
+++ Test_BST.py
@@ -0,0 +1,15 @@
+from Tree.BinarySearchTree import BST
+
+bst = BST()
+
+bst.add2(9)
+bst.add2(3)
+bst.add2(4)
+bst.add2(8)
+bst.add2(20)
+bst.add2(11)
+bst.add2(30)
+bst.add2(12)
+bst.add2(1)
+
+bst.dispay3(bst._head)
